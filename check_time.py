56cb2b2939d7c86f7a5424cc4f3707c1308e239c
GR <gustavo.angel.fr.96@gmail.com>
2026-07-19 12:25:33 -0600

    Compila, ejecuta, compara e imprime en una sola pasada

--- check_time.py
+++ check_time.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 # pyrefly: ignore [untyped-import]
 from tabulate import tabulate
+from tqdm import tqdm
 
 def check_times_file (dir_times: str) -> None:
   if os.path.exists(dir_times):
@@ -14,11 +15,11 @@ def check_times_file (dir_times: str) -> None:
       archivo.write("")
 
 def medir_tiempo(directorio: str, comando: list, repeticiones: int, fortran: bool = False) -> tuple[list[float], float]:
-    # Cambiar de directorio de forma segura
+    #? Cambiar de directorio de forma segura
     original_dir: str = os.getcwd()
     os.chdir(directorio)
 
-    # 1. Warm-up (Lanzar una vez para cargar en caché)
+    #? 1. Warm-up (Lanzar una vez para cargar en caché)
     if fortran:
       try:
         subprocess.run("del uv.txt")
@@ -28,11 +29,11 @@ def medir_tiempo(directorio: str, comando: list, repeticiones: int, fortran: boo
     subprocess.run(comando, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     tiempos: list[float] = []
-    for _ in range(repeticiones):
-        # 2. Usar perf_counter para alta precisión
+    for _ in tqdm(range(repeticiones)):
         if fortran:
           subprocess.run("del uv.txt")
           subprocess.run("del xy.txt")
+        #? 2. Usar perf_counter para alta precisión
         start: float = time.perf_counter()
         subprocess.run(comando, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         end: float = time.perf_counter()
@@ -44,6 +45,41 @@ def medir_tiempo(directorio: str, comando: list, repeticiones: int, fortran: boo
     return (tiempos, sum(tiempos) / len(tiempos))
 
 if __name__ == "__main__":
+  #! Bandera para compilar
+  compilar = False
+
+  if compilar:
+    print("Compilando...")
+    original_dir: str = os.getcwd()
+    
+    print("Cpp: ", end="")
+    start: float = time.perf_counter()
+    os.chdir("Cpp")
+    if not os.path.exists("build"):
+      os.mkdir("build")
+    subprocess.run("clang++ main.cpp -std=c++23 -O3 -o build\\main.exe", shell=True, stderr=subprocess.DEVNULL)
+    os.chdir(original_dir)
+    end: float = time.perf_counter()
+    print(end - start)
+
+    print("Fortran: ", end="")
+    start: float = time.perf_counter()
+    os.chdir("Fortran90")
+    if not os.path.exists("build"):
+      os.mkdir("build")
+    subprocess.run("flang main.f90 -O3 -o build\\main.exe", shell=True, stderr=subprocess.DEVNULL)
+    os.chdir(original_dir)
+    end: float = time.perf_counter()
+    print(end - start)
+
+    print("Rust: ", end="")
+    start: float = time.perf_counter()
+    os.chdir("Rust")
+    subprocess.run("cargo build --release", shell=True, stderr=subprocess.DEVNULL)
+    os.chdir(original_dir)
+    end: float = time.perf_counter()
+    print(end - start)
+
   _repetitions = 10
 
   #! fortran
@@ -112,40 +148,20 @@ if __name__ == "__main__":
     headers=["Tiempo F90", "Tiempo Cpp", "Tiempo Rust"],
     tablefmt="grid")
   )
-  print(tabulate(
-    [
-      ["f90", media_f90],
-      ["c++", media_cpp],
-      ["rust", media_rust]
-    ],
-    headers=["Lenguaje", "Media"],
-    tablefmt="grid",
-    stralign="center",
-  ))
-  """
-  tiempos_generales: list[list[float]] = [
-    [tiempos_f90[i], tiempos_rust[i]]
-    for i in range(_repetitions)
+  tabla = [
+    ["f90", media_f90],
+    ["c++", media_cpp],
+    ["rust", media_rust]
   ]
 
+  tabla.sort(key=lambda x: x[1])
+  print(tabla)
+  for i in range(len(tabla)):
+    tabla[i].append(f"X{tabla[i][1]/tabla[0][1]:.3}")
   print(tabulate(
-    tiempos_generales,
-    headers=["Tiempo F90", "Tiempo Rust"],
-    tablefmt="grid")
-  )
-  print(tabulate(
-    [
-      ["f90", media_f90],
-      ["rust", media_rust]
-    ],
-    headers=["Lenguaje", "Media"],
+    tabla,
+    headers=["Lenguaje", "Media", "Comparativa"],
     tablefmt="grid",
     stralign="center",
   ))
-
-  print(
-    "Rust - Fortran = {:.4} - {:.4} = {:.4} ".format(
-      media_rust, media_f90, media_rust-media_f90
-    )
-  )
-  """
+  
